a24e32a03a58596fb4995c95ca4bc72ae322e4d0
sanjeev hegde <sanjeevh93@github.com>
2022-07-25 12:48:40 +0530

    added sample project

---
+++ src/main/java/com/mvnregistry/javaproject/JavaprojectApplication.java
@@ -0,0 +1,11 @@
+package com.mvnregistry.javaproject;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaprojectApplication {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
