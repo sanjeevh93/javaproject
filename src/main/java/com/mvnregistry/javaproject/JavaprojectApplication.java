6eb6ea01a7bd2dc5bf729331afddeec7e1587160
sanjeev hegde <sanjeevh93@github.com>
2022-07-26 19:04:34 +0530

    updated snapshot

--- src/main/java/com/mvnregistry/javaproject/JavaprojectApplication.java
+++ src/main/java/com/mvnregistry/javaproject/JavaprojectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaprojectApplication {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World 2");
 	}
 
 }
